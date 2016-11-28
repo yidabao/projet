0683707e71dcb85e7bd3447941cc2b3bff36189b
yida.bao <yida.bao@utbm.fr>
2016-11-28 16:17:38 -0500

    bb

--- test/models/city_test.rb
+++ test/models/city_test.rb
@@ -7,27 +7,5 @@ class CityTest < ActiveSupport::TestCase
    @cityWrong = cities(:two)
  end
  
- test "should create city" do
-   VCR.use_cassette("nominatim") do
-    assert_difference('City.count') do
-     city = City.new  name: @city.name
-     city.save
-   end
- end
-end
-  
-   test 'valid city' do
-     VCR.use_cassette("nominatim1") do
-       incorrect = City.new name:cities(:one).name
-       assert weather == 'rain'
-     end
-  end
-
-  test 'weather forecast' do 
-     VCR.use_cassette("weather") do
-       weather = cities(:one).weather
-      
-       assert weather == 'rain'
-     end
-  end
+ 
 end
